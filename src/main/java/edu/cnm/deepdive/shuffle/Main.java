b62239b5bf2ca016e88d4403e4b1dde466953277
drewsbrews <143551649+drewsbrews@users.noreply.github.com>
2023-09-20 17:04:30 -0600

    wrote method to read and parse stdin

--- src/main/java/edu/cnm/deepdive/shuffle/Main.java
+++ src/main/java/edu/cnm/deepdive/shuffle/Main.java
@@ -1,10 +1,24 @@
 package edu.cnm.deepdive.shuffle;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
   public static void main(String[] args) {
+    int[] data = parseCommandLine(args); // TODO Change to parseInputStream()
+    System.out.println(Arrays.toString(data));
+    Shuffler shuffler = new Shuffler();
+    shuffler.shuffle(data);
+    System.out.println(Arrays.toString(data));
+  }
+
+  private static int[] parseCommandLine(String[] args) {
     int[] data = new int[args.length];
     int i = 0;
     try {
@@ -12,12 +26,29 @@ public class Main {
         data[i] = Integer.parseInt(args[i]);
       }
     } catch (NumberFormatException e) {
-      System.err.printf("Parsing failed! %s%n",e.getMessage());
+      System.err.printf("Parsing failed! %s%n", e.getMessage());
     }
     data = Arrays.copyOf(data, i);
-    System.out.println(Arrays.toString(data));
-    Shuffler shuffler = new Shuffler();
-    shuffler.shuffle(data);
-    System.out.println(Arrays.toString(data));
+    return data;
+  }
+
+  private static int[] parseStandardInput() throws IOException {
+    List<Integer> data = new LinkedList<>();
+    Reader reader = new InputStreamReader(System.in);
+    BufferedReader buffer = new BufferedReader(reader);
+    String input;
+    try {
+      while ((input = buffer.readLine()) != null) {
+        data.add(Integer.parseInt(input.strip()));
+      }
+    } catch (NumberFormatException e) {
+      System.err.printf("Parsing failed! %s%n", e.getMessage());
+    }
+    int[] result = new int[data.size()];
+    int position = 0;
+    for (int value : data) {
+      result[position++] = value;
+    }
+    return result;
   }
 }
